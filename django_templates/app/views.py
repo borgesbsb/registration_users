5b72c6a58e0af98f487a8201db18e7a70c4e0281
Benjamin  da Silva Borges <borges.bnjamin@gmail.com>
2023-01-05 22:46:49 -0400

    add: templates

--- django_templates/app/views.py
+++ django_templates/app/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    nome_da_empresa = "Study with python"
+    descricao = "Estudo de reciclagem de desenvolvimento de sistemas"
+    contato_empresa = {
+         "endereco": "Rua 03, numero 06, Vila Marinho, Compensa 3 - Manaus/AM",
+         "telefone": "92982472705",
+         "email": "borgesbsb.dev@gmail.com"
+    }
+    return render(request,'index.html', {'nome_empresa': nome_da_empresa,'descricao':descricao, 'contato_empresa':contato_empresa} )

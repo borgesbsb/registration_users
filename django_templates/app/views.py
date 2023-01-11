da17ea5b9c95c2bb65bc9c8c4e674f6cec89942c
Benjamin  da Silva Borges <borges.bnjamin@gmail.com>
2023-01-10 22:06:26 -0400

    add: loops and tags

--- django_templates/app/views.py
+++ django_templates/app/views.py
@@ -10,7 +10,13 @@ def index(request):
          "telefone": "92982472705",
          "email": "borgesbsb.dev@gmail.com"
     }
-    return render(request,'index.html', {'nome_empresa': nome_da_empresa,'descricao':descricao, 'contato_empresa':contato_empresa} )
+    
+    cursos_home = {
+        1: {"titulo": "Django Fundamentos","descricao": "Aprenda toda a base do Django agora mesmo!!"},
+        2: {"titulo": "Flask Fundamentos","descricao": "Aprenda toda a base do Flask agora mesmo!!"},
+        3: {"titulo": "Python OO","descricao": "Aprenda orientação a objetos com python"}
+    }
+    return render(request,'index.html', {'nome_empresa': nome_da_empresa,'descricao':descricao, 'contato_empresa':contato_empresa, 'cursos_home':cursos_home, } )
 
 def about(request):
     return HttpResponse("Pagina sobre")
